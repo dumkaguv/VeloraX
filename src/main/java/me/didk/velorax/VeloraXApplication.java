2f1e6da9016cb4ed80e93a47503944ce5cb08c76
Dmitrii Golovicichin <dmitrii.golovicichin@gmail.com>
2026-03-07 13:21:05 +0200

    chore(init): bootstrap spring boot project structure

---
+++ src/main/java/me/didk/velorax/VeloraXApplication.java
@@ -0,0 +1,13 @@
+package me.didk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloraXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeloraXApplication.class, args);
+    }
+
+}
